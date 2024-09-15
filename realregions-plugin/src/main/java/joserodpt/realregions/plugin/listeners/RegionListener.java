cf24266f97c2d714f84624bf12cc5fe087b50697
José Rodrigues <joser0drigueswork@gmail.com>
2024-09-15 12:55:19 +0100

    New version notification change

--- realregions-plugin/src/main/java/joserodpt/realregions/plugin/listeners/RegionListener.java
+++ realregions-plugin/src/main/java/joserodpt/realregions/plugin/listeners/RegionListener.java
@@ -65,7 +65,7 @@ public class RegionListener implements Listener {
     @EventHandler
     public void onJoin(final PlayerJoinEvent e) {
         if (e.getPlayer().isOp() && rr.hasNewUpdate()) {
-            Text.send(e.getPlayer(), "&6&LWARNING! &r&fThere is a new update available for Real&eRegions&f! https://www.spigotmc.org/resources/111629/");
+            Text.send(e.getPlayer(), "&6&LWARNING! &r&fThere is a new update available! https://www.spigotmc.org/resources/111629/");
         }
     }
 
